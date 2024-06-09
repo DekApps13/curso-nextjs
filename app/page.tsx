873a312a6512c156cc0bfea6ab10f32d5aca2f46
DekApps13 <derekvillarreal60@gmail.com>
2024-06-09 00:47:21 -0500

    bugfix/status property

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts'
-
-
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-
 import Link from 'next/link';
 import AcmeLogo from '@/app/ui/acme-logo'
 import Image from 'next/image'
 
-export default function Page({ status }: { status: any }) {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -33,20 +30,19 @@ export default function Page({ status }: { status: any }) {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-          <Image 
+          <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshot del Dashboard V-Desktop"
           />
-          <Image 
+          <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
             className="block md:hidden"
             alt="Screenshot del Dashboard V-Mobile"
-            
           />
         </div>
       </div>
