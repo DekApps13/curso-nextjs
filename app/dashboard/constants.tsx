7d2ae10e0cbf3a2797395e5024d82078b7c0652b
DekApps13 <derekvillarreal60@gmail.com>
2024-06-30 21:10:04 -0500

    postgree

---
+++ app/dashboard/constants.tsx
@@ -0,0 +1,13 @@
+const constants = () => {
+  const DJ = {
+    declaracionJurada: {
+      title: "Declaración Jurada",
+      content: "",
+      version: 1
+    }
+  }
+
+  return {DJ};
+}
+
+export default constants;
